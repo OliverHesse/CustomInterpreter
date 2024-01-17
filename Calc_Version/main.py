fcc5e96262fd2afe4f20da6063cd7b8c0c7cf8a8
lunarLucent <109795653+lunarLucent@users.noreply.github.com>
2024-01-17 18:50:40 +0000

    you can now input new functions

--- Calc_Version/main.py
+++ Calc_Version/main.py
@@ -1,79 +1,63 @@
-class Token():
+import os
+import time
+from lexer import Token,lexer
 
-  def __init__(self, type, value):
-    self.value = value
-    self.type = type
-
-  def __str__(self):
-    return f"Token({self.type},{self.value})"
-
-  def __repr__(self) -> str:
-    return self.__str__()
-
-
-class lexer():
-
+class Lexer():
   def __init__(self, text):
     self.text = text
     self.pos = 0
     self.current_char = text[0]
     
-
   def get_next_char(self):
     if self.pos + 1 >= len(self.text):
       return "EOF"
     else:
       self.pos += 1
       return self.text[self.pos]
-
+    
   def peek(self):
     if self.pos + 1 > len(self.text) - 1:
       return "EOF"
     else:
       return self.text[self.pos + 1]
-
+    
+  def peekBehind(self):
+    if self.pos -1 < 0:
+      return None
+    else:
+      return self.text[self.pos - 1]
+    
   def tokenize(self):
     op_dic = {"+": "PLUS", "-": "MINUS", "/": "DIV", "*": "MULTI"}
     tokens = []
-
     while self.current_char != "EOF":
-      #print(self.current_char)
-      if (self.current_char == "-" and self.peek().isdigit()) and (len(tokens) == 0 or tokens[-1].type == "OP"):
-        #accounts for cases like 2+-3
-        self.current_char = self.get_next_char()
-        current_num = int(self.current_char)
-
+      #first check if its a digit or -digit
+      if self.current_char.isdigit() or (self.current_char == "-" and (self.peekBehind() in op_dic or self.peekBehind() is None)):
+        num = self.current_char
         while self.peek().isdigit():
-
           self.current_char = self.get_next_char()
-
-          current_num = current_num * 10 + int(self.current_char)
-        tokens.append(Token("INT", -current_num))
-      elif self.current_char.isdigit():
-        #i have encounterd a number, start creating a new num
-        current_num = int(self.current_char)
-
-        while self.peek().isdigit():
-
-          self.current_char = self.get_next_char()
-
-          current_num = current_num * 10 + int(self.current_char)
-
-        tokens.append(Token("INT", current_num))
-      if self.current_char in op_dic:
-        #we have an operator
+          num += self.current_char
+       
+        tokens.append(Token("INT",int(num)))
+      elif self.current_char in op_dic:
+        #this is an operator
         tokens.append(Token("OP", self.current_char))
-      if self.current_char == "(":
-        tokens.append(Token("OpenBracket", self.current_char))
-      if self.current_char == ")":
-        tokens.append(Token("CloseBracket", self.current_char))
-
+      elif self.current_char == "(" or self.current_char == ")":
+        bracket_map = {"(":"OpenBracket",")":"ClosedBracket"}
+        tokens.append(Token(bracket_map[self.current_char], self.current_char))
+      else:
+        #found a general character will either be a variable or a function
+        #end will either be an open bracket or an operator 
+        current_value = self.current_char
+        while self.peek() != "(" and self.peek() not in op_dic and self.peek() != "EOF":
+          self.current_char = self.get_next_char()
+          current_value += self.current_char
+        if self.peek() == "(":
+          tokens.append(Token("Function",current_value))
+        else:
+          tokens.append(Token("Variable",current_value))
       self.current_char = self.get_next_char()
-
-    tokens.append(Token("EOF", None))
     return tokens
-
-
 class Interpreter():
 
   def __init__(self, tokens):
@@ -178,8 +162,67 @@ class Interpreter():
     return nums[0]
 
 
-if __name__ == "__main__":
+def mainLoop():
+  global functions_map
+  functions_map = {}
+  while True:
+    delay = 0
+    text = input("(1)calculate (2)input function (3) view functions")
+    if text == "1":
+      delay = calculate()
+    elif text == "2":
+      delay = inputFunction()
+    elif text == "3":
+      delay = viewFunctions()
+    else:
+      print("please select a valid option")
+      delay = 2
+    
+    time.sleep(delay)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    delay = 0
 
+def viewFunctions():
+  for key,val in functions_map.items():
+    
+    expression = ""
+    for token in val["tokens"]:
+      expression += str(token.value)
+    print(f"{key}({val['variables']}) = {expression}")
+  input("enter to exit>")
+  return 0
+def calculate():
+  pass
+
+def inputFunction():
+  function_name = input("func name> ")
+  if function_name.find("(") == -1 or function_name.find(")") == -1 or function_name.find(" ") == -1 or function_name in functions_map:
+    print("error invalid function name or already present")
+    return 2
+  variables = []
+  done = False
+  while done == False:
+    new_variable = input("please enter the name of your variable")
+    if new_variable.isdigit() and new_variable.find("(") != -1 and new_variable.find(")") != -1 and new_variable.find(" ") != -1 :
+      print("error invalid variable name")
+      return 2
+    variables.append(new_variable)
+    action = input("new variable? (y/n)")
+    if action == "n":
+      done = True
+
+  contents = input(f"{function_name}({variables}) = " )
+  #tokenize the contents
+  #change tokenize to detect functions and variables 
+  new_func = Lexer(contents).tokenize()
+  functions_map[function_name] = {"variables":new_variable,"tokens":new_func}
+  return 0    
+if __name__ == "__main__":
+  test_input = "2-3+-2/4+x-sin(4)"
+  test_lexer = Lexer(test_input)
+  print(test_lexer.tokenize())
+  mainLoop()
+  
   while True:
     try:
       text = input("calc> ")
